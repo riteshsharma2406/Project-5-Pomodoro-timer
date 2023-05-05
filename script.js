fb6240ee02d9a495144815de693a39f8596f6f02
Ritesh Sharma <101566115+riteshsharma2406@users.noreply.github.com>
2023-05-05 19:07:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const startEl = document.getElementById("start");
+const stopEl = document.getElementById("stop");
+const resetEl = document.getElementById("reset");
+const timerEl = document.getElementById("timer")
+
+let interval
+let timeLeft = 10;
+
+function updateTimer()
+{
+    let minutes = Math.floor(timeLeft/60);
+    let seconds = timeLeft%60;
+
+    let formatedTime = `${minutes.toString().padStart(2,"0")}:${seconds.toString().padStart(2,"0")}`;
+
+    timerEl.innerHTML = formatedTime;
+}
+
+function startTimer()
+{
+    interval = setInterval(()=>{
+        timeLeft--;
+        updateTimer();
+        if(timeLeft===0)
+        {
+            clearInterval(interval);
+            alert("Time's Up!")
+            timeLeft = 1500;
+            updateTimer();
+        }
+    },1000);
+}
+
+function stopTimer()
+{
+    clearInterval(interval);
+}
+
+function resetTImer()
+{
+    clearInterval(interval);
+    timeLeft = 1500;
+    updateTimer();
+}
+
+
+startEl.addEventListener("click",startTimer);
+stopEl.addEventListener("click",stopTimer);
+resetEl.addEventListener("click",resetTImer);
